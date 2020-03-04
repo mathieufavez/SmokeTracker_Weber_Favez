6cd916d6f796629f116112f00d6d2b80e1c79e61
Mathieu <mathieufavez1996@hotmail.com>
2020-03-04 14:41:22 +0100

    Images

--- app/src/main/java/com/example/smoketracker_weber_favez/HomeActivity.java
+++ app/src/main/java/com/example/smoketracker_weber_favez/HomeActivity.java
@@ -2,13 +2,24 @@ package com.example.smoketracker_weber_favez;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private Button mNextButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mNextButton = (Button) findViewById(R.id.button_next_homeActivity);
+        mNextButton.setEnabled(false);
+        //mNextButton.setOnClickListener();
+
     }
+
 }
--- app/src/main/java/com/example/smoketracker_weber_favez/TrackingActivity.java
+++ app/src/main/java/com/example/smoketracker_weber_favez/TrackingActivity.java
@@ -10,6 +10,6 @@ public class TrackingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tracking);
-        //Test
+
     }
 }

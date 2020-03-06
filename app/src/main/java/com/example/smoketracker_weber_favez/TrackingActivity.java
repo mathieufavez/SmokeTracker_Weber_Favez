f66cabb17539412376f7554bbd7c87c8c7b1cffb
Mathieu <mathieufavez1996@hotmail.com>
2020-03-06 12:20:32 +0100

    Fragments done

--- app/src/main/java/com/example/smoketracker_weber_favez/TrackingActivity.java
+++ app/src/main/java/com/example/smoketracker_weber_favez/TrackingActivity.java
@@ -1,8 +1,11 @@
 package com.example.smoketracker_weber_favez;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -13,7 +16,37 @@ public class TrackingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tracking);
 
+        BottomNavigationView bottomNavigationView = findViewById(R.id.activity_tracking_bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.activity_tracking_frame_layout, new TrackingFragment()).commit();
     }
 
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
 
+                    switch(item.getItemId()){
+                        case R.id.nav_tracking:
+                            selectedFragment = new TrackingFragment();
+                            break;
+                        case R.id.nav_history:
+                            selectedFragment = new HistoryFragment();
+                            break;
+                        case R.id.nav_stats:
+                            selectedFragment = new StatsFragment();
+                            break;
+                        case R.id.nav_account:
+                            selectedFragment = new AccountFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.activity_tracking_frame_layout,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
 }

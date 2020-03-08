9b89e6deff0301cea7284f4bcdff14078710bcc1
Mathieu <mathieufavez1996@hotmail.com>
2020-03-08 20:28:44 +0100

    AccountActivity Base ok

--- app/src/main/java/com/example/smoketracker_weber_favez/AccountFragment.java
+++ app/src/main/java/com/example/smoketracker_weber_favez/AccountFragment.java
@@ -1,9 +1,11 @@
 package com.example.smoketracker_weber_favez;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,10 +13,28 @@ import androidx.fragment.app.Fragment;
 
 public class AccountFragment extends Fragment {
 
+    private Button modifyButton;
+    private Button startOverButton;
+    private Button modifyTablesButton;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_account, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_account, container, false);
+        modifyButton = (Button) view.findViewById(R.id.button_modify);
+
+
+        modifyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Launch the next activity
+                Intent homeActivityIntent = new Intent(getContext(), HomeActivity.class);
+                startActivity(homeActivityIntent);
+                // }
+
+            }
+        });
+        return view;
     }
 }
 

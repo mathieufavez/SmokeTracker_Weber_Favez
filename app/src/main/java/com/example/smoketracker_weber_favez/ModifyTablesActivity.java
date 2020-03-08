50e7253ec1d28623776d22f181b119251ca042ff
liamw <liam.weber@outlook.com>
2020-03-08 20:20:52 +0100

    Welcome Screen done

---
+++ app/src/main/java/com/example/smoketracker_weber_favez/ModifyTablesActivity.java
@@ -0,0 +1,50 @@
+package com.example.smoketracker_weber_favez;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ModifyTablesActivity extends AppCompatActivity {
+
+    private Button userButton;
+    private Button dayButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modify_tables);
+
+        userButton = (Button) findViewById(R.id.button_user);
+
+
+        userButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Launch the next activity
+                Intent userIntent = new Intent(ModifyTablesActivity.this, HomeActivity.class);
+                startActivity(userIntent);
+                // }
+
+            }
+        });
+
+        dayButton = (Button) findViewById(R.id.button_day);
+
+
+        dayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Launch the next activity
+                Intent dayIntent = new Intent(ModifyTablesActivity.this, HomeActivity.class);
+                startActivity(dayIntent);
+                // }
+
+            }
+        });
+    }
+}
+
